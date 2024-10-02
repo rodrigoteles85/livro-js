7c491cad7b40c514e08e85f984b9578c15bf4c55
Rodrigo Teles <rodrigo.teles85@gmail.com>
2024-10-02 09:54:23 -0300

    quinto exercicio do livrojs

---
+++ cap1/ex1_2.1/app.js
@@ -0,0 +1,12 @@
+const a = '20'
+const b = a * 2 // b = 40
+const c = a / 2 // c = 10
+const d = a - 2 // d = 18
+const e = a + 2 // e = 202
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+alert(`e: ${e}`)
