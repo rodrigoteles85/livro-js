35546c4739bdaa5a316220cb2b1426e5e6d4b29d
Rodrigo Teles <rodrigo.teles85@gmail.com>
2024-10-06 12:28:05 -0300

    decimo quarto exercicio do livrojs

---
+++ cap1/ex1_11.4/app.js
@@ -0,0 +1,7 @@
+const nota1 = Number(prompt('1ª nota: '))
+const nota2 = Number(prompt('2ª nota: '))
+const media = (nota1 + nota2) / 2
+
+console.log(`1ª nota: ${nota1}`)
+console.log(`2ª nota: ${nota2}`)
+console.log(`Média: ${media}`)
