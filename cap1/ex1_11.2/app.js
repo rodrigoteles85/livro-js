33fa7d3859f8f7c60ff67f769f50919b6d3da9f2
Rodrigo Teles <rodrigo.teles85@gmail.com>
2024-10-06 12:05:43 -0300

    decimo segundo exercicio do livrojs

---
+++ cap1/ex1_11.2/app.js
@@ -0,0 +1,5 @@
+const conta = Number(prompt('Valor da conta: R$'))
+const clientes = Number(prompt('Número de clientes: '))
+const valorPorClientes = conta / clientes
+
+alert(`Valor por clientes R$${valorPorClientes}`)
