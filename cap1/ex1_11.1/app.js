0020287aedf468215f06c18c7f69e642bccba463
Rodrigo Teles <rodrigo.teles85@gmail.com>
2024-10-06 11:54:36 -0300

    decimo primeiro exercicio do livrojs

---
+++ cap1/ex1_11.1/app.js
@@ -0,0 +1,5 @@
+// Elaborar um programa que leia um número. Calcule e informe os seus vizinhos, ou seja, o número anterior e posterior
+
+const num = Number(prompt('Número: '))
+
+alert(`Vizinhos: ${num - 1} e ${num + 1}`)
